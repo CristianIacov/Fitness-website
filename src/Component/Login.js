d2b800fee24611c1ebe9372c8d74b3d7dda0df20
CristianIacov <cristian.iacov1998@yahoo.com>
2020-03-31 21:34:41 +0300

    First commit

---
+++ src/Component/Login.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+  return (
+ 
+                   <nav>
+            <h1>Lauren Moor</h1>
+            <ul>
+                <li>Home</li>
+               
+           
+
+            </ul>
+        </nav>
+    
+  );
+
+
+export default Login;
